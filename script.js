dfba531e47126de339532869a6b95402a5c3eaef
Sumit Kumar <smitk1057@gmail.com>
2026-04-01 10:43:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
- let  getSumBtn = document.getElementById("getSumBtn");
+ let  getSumBtn = document.getElementById("#getSumBtn");
 
 getSumBtn.addEventListener("click", function () {
   let sum = 0;
 
-  const prices = document.querySelectorAll(".prices");
+  let  prices = document.querySelectorAll(".prices");
 
   prices.forEach(price => {
     sum += Number(price.innerText);
   });
 
-  const table = document.getElementById("table");
+  let table = document.getElementById("table");
 
-  const tr = document.createElement("tr");
+  let tr = document.createElement("tr");
   tr.id = "ans";
 
   const td = document.createElement("td");
